130e415d7ca9ff8fde8410f1d2b4693c7b4ce7dc
comsong <comsong@comsongui-MacBook-Pro.local>
2020-03-27 08:10:21 +0900

    release new version of comsong-maven-repo

---
+++ src/main/java/Utils.java
@@ -0,0 +1,11 @@
+public class Utils {
+    private String name = "comsong";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
